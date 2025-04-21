fcb75e046170bf758d8c0a783fe79363a166db0e
WTF78 <50106339+WTF78@users.noreply.github.com>
2025-04-21 19:48:53 +0300

    modify config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    basePath: '/relax',
     output: "export",
     distDir: "dist",
     images: {
